5e52bd3cb30b2d9589bbbe3ab1b3a6b639ba2cef
Lian Liu <lian.liu@addepar.com>
2018-08-01 17:29:10 -0700

    hide and show logic

--- datepicker-app/src/components/AddeDatePicker/AddeDatePicker.js
+++ datepicker-app/src/components/AddeDatePicker/AddeDatePicker.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react';
 import AdvancedMode from '../AdvancedMode/AdvancedMode';
 import DateRangePicker from '../DateRangePicker/DateRangePicker';
+import Input from './Input';
 
 class AddeDatePicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showAdvancedMode: false,
+      showDateRangePicker: false,
+      startDate: '',
+      endDate: '',
+    };
+
+    this.onFocusHandler = this.onFocusHandler.bind(this);
+    this.onBlurHandler = this.onBlurHandler.bind(this);
+  }
+
+  renderDateRangePicker() {
+    if(this.state.showDateRangePicker) {
+      return <DateRangePicker />;
+    }
+    return null;
+  }
+
+  renderAdvancedMode() {
+    if(this.state.showAdvancedMode) {
+      return <AdvancedMode />;
+    }
+    return null;
+  }
+
+  onFocusHandler() {
+    this.setState({
+      showDateRangePicker: true,
+      showAdvancedMode: false,
+    });
+  }
+
+  onBlurHandler() {
+    this.setState({
+      showDateRangePicker: false,
+      showAdvancedMode: false,
+    });
+  }
+
   render() {
     return (
       <div className='addeDatePicker'>
         <div>
-          <input />
+          <Input 
+            onFocus={this.onFocusHandler}
+            onBlur={this.onBlurHandler}
+          />
         </div>
-        <DateRangePicker />
-        <AdvancedMode />
+        { this.renderAdvancedMode() }
+        { this.renderDateRangePicker() }
       </div>
     );
   }
---
+++ datepicker-app/src/components/AddeDatePicker/Input.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='input'>
+        <input 
+          onFocus={this.props.onFocus}
+          onBlur={this.props.onBlur}
+        />
+      </div>
+    );
+  }
+}
+
+export default Input;
