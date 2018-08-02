be7082031f522c0d650cae23d1ea82383e56ceb2
Chin Wei Wong <chin-wei.wong@addepar.com>
2018-08-02 10:05:33 -0700

    replaced with day picker input, fixed layout issues

--- datepicker-app/src/components/DateRangePicker/DateRangePicker.js
+++ datepicker-app/src/components/DateRangePicker/DateRangePicker.js
@@ -1,43 +1,65 @@
 import React, { Component } from 'react';
-import DayPickerInput from 'react-day-picker/DayPickerInput';
+import DayPicker from 'react-day-picker/DayPicker';
 import 'react-day-picker/lib/style.css';
 
 
 export default class DateRangePicker extends React.Component {
   constructor(props) {
     super(props);
-    this.handleDayChange = this.handleDayChange.bind(this);
+    this.selectedStartDay = this.selectedStartDay.bind(this);
+    this.selectedEndDay = this.selectedEndDay.bind(this);
     this.state = {
-      selectedDay: undefined,
+      selectedStartDay: undefined,
+      selectedEndDay: undefined,
     };
   }
-  handleDayChange(day) {
-    this.setState({ selectedDay: day });
+
+  selectedStartDay(day, { selected }) {
+    this.setState({
+      selectedStartDay: selected ? undefined : day,
+    });
   }
+
+  selectedEndDay(day, { selected }) {
+    this.setState({
+      selectedEndDay: selected ? undefined : day,
+    });
+  }
+
+
   render() {
-    const { selectedDay } = this.state;
+    const { selectedStartDay, selectedEndDay } = this.state;
     return (
       <div className='dateRangePicker'>
-        <div className='start-date'>
-          <label>Start Date</label>
-          <DayPickerInput
-            onDayChange={this.handleDayChange}
-            showOverlay={true}
-            hideOnDayClick={false}
-            showDayPicker={true}
-          />
+        <div className="dateRangePicker-quicklinks">
+          <button className="button-text">YTD</button>
+          <button className="button-text">MTD</button>
+          <button className="button-text">WTD</button>
         </div>
+        <div className="dateRangePicker-content">
+          <div className='date-picker-wrapper'>
+            <label className="input-label">Start Date</label>
 
-        <div className='end-date'>
-        <label>End Date</label>
-        <DayPickerInput
-          onDayChange={this.handleDayChange}
-          showOverlay={true}
-          hideOnDayClick={false}
-          showDayPicker={true}
-        />
+            <input type="text" value={this.state.selectedStartDay}/>
+
+            <DayPicker
+              selectedDays={this.state.selectedStartDay}
+              onDayClick={this.selectedStartDay}
+            />
+
+          </div>
+
+          <div className='date-picker-wrapper'>
+          <label className="input-label">End Date</label>
+          <input type="text" value={this.state.selectedEndDay}/>
+
+            <DayPicker
+              selectedDays={this.state.selectedEndDay}
+              onDayClick={this.selectedEndDay}
+            />
+          </div>
+
         </div>
-
         <footer className="cta-footer">
           <button className="button-primary">Apply</button>
           <button className="button-default">Cancel</button>
