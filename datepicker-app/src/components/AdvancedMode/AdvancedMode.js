9f6e669901b5dda69a4ede3ce9222693d9d321c7
joelshawaddepar <joel.shaw@addepar.com>
2018-08-02 17:41:29 -0700

    Merging WIP Styling

--- datepicker-app/src/components/AdvancedMode/AdvancedMode.js
+++ datepicker-app/src/components/AdvancedMode/AdvancedMode.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class AdvancedMode extends Component {
   render() {
     return (
-      <div className='advancedMode'>
-        This is the Advanced Mode.
+      <div className='advancedMode datepicker-query-search'>
+        <div className="state-empty">
+          <p>Start typing an expression - e.g. t-3</p>
+        </div>
+
+        <div className="state-no-match">
+          <p>t-</p>
+          <p className="small">No matches yet</p>
+        </div>
       </div>
     );
   }
